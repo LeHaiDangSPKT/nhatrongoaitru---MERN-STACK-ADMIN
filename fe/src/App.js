e3c58e8caedd75a73547ff2f0f6af31bb934b40d
LeHaiDangSPKT <20110243@student.hcmute.edu.vn>
2022-10-28 05:29:30 +0700

    .

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,9 +1,7 @@
 import * as React from "react";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Content_main from "./components/Content_main";
 import { Routes, Route } from "react-router-dom";
-import Streets from "./components/Streets/Streets";
 import Manager from "./components/Manager/Manager";
 import ManagerActivity from "./components/Manager/Activity/ManagerActivity";
 
@@ -14,31 +12,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="*" element={<Content_main />} />
-        <Route path="/TPDiAn" element={<Streets name="TPDiAn" />} />
-        <Route path="/LinhDong" element={<Streets name="LinhDong" />} />
-        <Route path="/LinhTrung" element={<Streets name="LinhTrung" />} />
-        <Route path="/LinhChieu" element={<Streets name="LinhChieu" />} />
-        <Route path="/TangNhonPhuA" element={<Streets name="TangNhonPhuA" />} />
-        <Route path="/TanPhu" element={<Streets name="TanPhu" />} />
-        <Route path="/BinhTho" element={<Streets name="BinhTho" />} />
-        <Route path="/HiepPhu" element={<Streets name="HiepPhu" />} />
-        <Route path="/TruongTho" element={<Streets name="TruongTho" />} />
-        <Route path="/TangNhonPhuB" element={<Streets name="TangNhonPhuB" />} />
-        <Route path="/LongThanhMy" element={<Streets name="LongThanhMy" />} />
-        <Route path="/PhuocLongA" element={<Streets name="PhuocLongA" />} />
-        <Route path="/PhuocLongB" element={<Streets name="PhuocLongB" />} />
-        <Route
-          path="/HiepBinhChanh"
-          element={<Streets name="HiepBinhChanh" />}
-        />
-        <Route path="/PhuHuu" element={<Streets name="PhuHuu" />} />
-        <Route path="/LinhTay" element={<Streets name="LinhTay" />} />
         {check && <Route path="/manager/" element={<Manager />} />}
         {check && (
           <Route path="/managerActivity/" element={<ManagerActivity />} />
         )}
       </Routes>
-      <Footer />
     </div>
   );
 }
